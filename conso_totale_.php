50ea64de435431a57fc63d5c05e9bc9e557b71c4
AntorFR <contact@antor.fr>
2016-07-13 21:30:35 +0200

    V2
    utilisation du script getPeriphList plutôt que de passer par l’API +
    Reload auto tous les jours

--- conso_totale_.php
+++ conso_totale_.php
@@ -3,26 +3,16 @@ $idperiph = getArg('idperiph');
 $operation = getArg('operation',false,NULL);
 $print = getArg('print',false,false);
     
-//-- Reload auto each day
-$CACHE_DURATION = 24*60; // minutes
-$last_reload = loadVariable('last_reload');
-if ((time() - $last_reload) / 60 > $CACHE_DURATION)
-{
-    $operation = "reload";
-}
-//--
-    
 switch ( $operation ) {
         case "reload" :
             $conso_devices = array();
             $list = getPeriphList();
             foreach ($list as $device){
-                if ($device['utilisation_id'] == 26 && $device['controller_module_id'] != $idperiph) {
+                if ($device['usage_id'] == 26 && $device['periph_id'] != $idperiph) {
                     $conso_devices[]= $device;
                 }
             }
             saveVariable('conso_devices', $conso_devices);
-            saveVariable('last_reload', time());
         
         default:
             $total = 0;
@@ -32,21 +22,21 @@ switch ( $operation ) {
             foreach ($conso_devices as $device){
                 
 
-                $value = getValue($device["controller_module_id"]);
+                $value = getValue($device["periph_id"]);
                 
-                if ($value["value"] > 0 && $device["parent_controller_module_id"]!= "") {
-                    $parent_value = getValue($device["parent_controller_module_id"]);
+                if ($value["value"] > 0 && $device["parent_periph_id"]!= "") {
+                    $parent_value = getValue($device["parent_periph_id"]);
                     
                     if ($parent_value["value"] == 0) {
-                        $value = getValue($device["controller_module_id"]);
-                        if ($print) $print_devices["devices"][$device["controller_module_id"]] = array("name" => $device["full_name"], "value" => "0 (".$value["value"].")");
+                        $value = getValue($device["periph_id"]);
+                        if ($print) $print_devices["devices"][$device["periph_id"]] = array("name" => $device["name"], "value" => "0 (".$value["value"].")");
                     } else {
                         $total += $value["value"];
-                        if ($print) $print_devices["devices"][$device["controller_module_id"]] = array("name" => $device["full_name"], "value" => $value["value"]);
+                        if ($print) $print_devices["devices"][$device["periph_id"]] = array("name" => $device["name"], "value" => $value["value"]);
                     }
                 } else {
                     $total += $value["value"];
-                    if ($print) $print_devices["devices"][$device["controller_module_id"]] = array("name" => $device["full_name"], "value" => $value["value"]);
+                    if ($print) $print_devices["devices"][$device["periph_id"]] = array("name" => $device["name"], "value" => $value["value"]);
                 }
                 
             }
@@ -62,7 +52,7 @@ function sdk_write_conso($print_devices) {
     print "<eedomus_consos>".PHP_EOL;
     print " <consos>".PHP_EOL;
     foreach ($print_devices["devices"] as $id => $device) {
-        print "	<conso id='".$id."'>".PHP_EOL;
+        print "	<conso id=".$id.">".PHP_EOL;
         print "		<name>".addslashes(utf8_encode($device["name"]))."</name>".PHP_EOL;
         print "		<value>".$device["value"]."</value>".PHP_EOL;
         print "	</conso>".PHP_EOL;
